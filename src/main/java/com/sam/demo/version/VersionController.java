e7f013459a7f8bee9a5f6bdffd5c3f5cb77420eb
DangKunpeng <kunpeng.dang@accenture.cn>
2025-12-05 09:43:50 +0800

    #inin configs

---
+++ src/main/java/com/sam/demo/version/VersionController.java
@@ -0,0 +1,20 @@
+package com.sam.demo.version;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class VersionController {
+
+    @RequestMapping(value = "/user", version = "1")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok("hello world");
+    }
+
+    @RequestMapping(value = "/user", version = "2")
+    public ResponseEntity<String> getVersion2() {
+        return ResponseEntity.ok("hello v2");
+    }
+}
