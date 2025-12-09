dd8bb622e999616a545a19896aeecb49a7d10bff
DangKunpeng <kunpeng.dang@accenture.cn>
2025-12-09 11:24:37 +0800

    #inin configs

---
+++ src/main/java/com/sam/vt/signin/RedisBitMapUtils.java
@@ -0,0 +1,44 @@
+package com.sam.vt.signin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+public class RedisBitMapUtils {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public Boolean setSigned(String userId, LocalDate date) {
+        if (getSigned(userId, date)) {
+            return false;
+        }
+        return stringRedisTemplate.opsForValue()
+                .setBit(getSignKey(userId, date), getOffset(date), true);
+    }
+
+    private Boolean getSigned(String userId, LocalDate date) {
+        return stringRedisTemplate.opsForValue()
+                .getBit(getSignKey(userId, date), getOffset(date));
+    }
+
+    private static int getOffset(LocalDate date) {
+        return date.getDayOfYear() - 1;
+    }
+
+    public Integer summary(String userId, LocalDate date) {
+        Integer count = 0;
+        while (getSigned(userId, date)) {
+            count++;
+            date = date.minusDays(1);
+        }
+        return count;
+    }
+
+    private static String getSignKey(String userId, LocalDate date) {
+        return date.getYear() + ":sign:" + userId;
+    }
+}
--- src/main/java/com/sam/vt/signin/SignRedisService.java
+++ src/main/java/com/sam/vt/signin/SignRedisService.java
@@ -1,9 +1,8 @@
 package com.sam.vt.signin;
 
 import com.sam.vt.signin.beans.Result;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -13,47 +12,19 @@ import static com.sam.vt.utils.Constants.SYS_DEFAULT_DAY_PATTERN;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class SignRedisService {
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+
+    private final RedisBitMapUtils redisBitMapUtils;
 
     public Result<String> sign(String userId, LocalDate date) {
-
-        String day = date.format(DateTimeFormatter.ofPattern(SYS_DEFAULT_DAY_PATTERN));
-        if (getSigned(userId, date)) {
-            return Result.error("Already signed in " + day);
-        }
         Thread.ofVirtual().start(() -> {
-            setSigned(userId, date);
-            log.info("on {}, total sign-ins: {}", day, summary(userId, date));
+            redisBitMapUtils.setSigned(userId, date);
+            log.info("on {}, total sign-ins: {}",
+                    date.format(DateTimeFormatter.ofPattern(SYS_DEFAULT_DAY_PATTERN)),
+                    redisBitMapUtils.summary(userId, date));
         });
         return Result.success("Sign-in successful");
     }
 
-    private void setSigned(String userId, LocalDate date) {
-        stringRedisTemplate.opsForValue()
-                .setBit(getSignKey(userId, date), getOffset(date), true);
-    }
-
-    private Boolean getSigned(String userId, LocalDate date) {
-        return stringRedisTemplate.opsForValue()
-                .getBit(getSignKey(userId, date), getOffset(date));
-    }
-
-    private static int getOffset(LocalDate date) {
-        return date.getDayOfYear() - 1;
-    }
-
-    private Integer summary(String userId, LocalDate date) {
-        Integer count = 0;
-        while (getSigned(userId, date)) {
-            count++;
-            date = date.minusDays(1);
-        }
-        return count;
-    }
-
-    private static String getSignKey(String userId, LocalDate date) {
-        return date.getYear() + ":sign:" + userId;
-    }
 }
