35957aedb1d70d79fa4cac7f564a1d7980b5a130
DangKunpeng <kunpeng.dang@accenture.cn>
2025-12-12 17:11:11 +0800

    #MDC

--- src/main/java/com/sam/vt/mdc/MdcLoggingFilter.java
+++ src/main/java/com/sam/vt/mdc/MdcLoggingFilter.java
@@ -1,6 +1,5 @@
 package com.sam.vt.mdc;
 
-import com.sam.vt.utils.AjaxUtils;
 import com.sam.vt.utils.ExceptionUtils;
 import jakarta.servlet.*;
 import lombok.extern.slf4j.Slf4j;
@@ -11,14 +10,14 @@ import java.io.IOException;
 public class MdcLoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        AjaxUtils.putTraceId();
+        MdcUtils.putTraceId();
         try {
             chain.doFilter(request, response);
         } catch (Exception e) {
             log.error("apiId set error {}", ExceptionUtils.getExceptionStackTrace(e));
         } finally {
             // 清理MDC信息以避免内存泄漏或错误传递
-            AjaxUtils.removeTraceId();
+            MdcUtils.removeTraceId();
         }
     }
 }
--- src/main/java/com/sam/vt/utils/AjaxUtils.java
+++ src/main/java/com/sam/vt/utils/AjaxUtils.java
@@ -1,16 +1,10 @@
 package com.sam.vt.utils;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.MDC;
 
 import java.util.Objects;
 
-@Log4j2
 public class AjaxUtils {
-
-
     /**
      * 验证是否是ajax请求
      *
@@ -23,26 +17,5 @@ public class AjaxUtils {
 
     public static boolean isAjaxUploadRequest(HttpServletRequest request) {
         return request.getParameter("ajaxUpload") != null;
-    }
-
-
-    public static String getTraceId() {
-        String traceId = MDC.get(SysDefaults.TRACE_ID);
-        if (StringUtils.isBlank(traceId)) {
-            putTraceId();
-        }
-        return MDC.get(SysDefaults.TRACE_ID);
-    }
-
-    public static void putTraceId() {
-        putTraceId(RedisHelper.newKey(SysDefaults.TRACE_ID));
-    }
-
-    public static void putTraceId(String traceId) {
-        MDC.put(SysDefaults.TRACE_ID, traceId);
-    }
-
-    public static void removeTraceId() {
-        MDC.remove(SysDefaults.TRACE_ID);
     }
 }
