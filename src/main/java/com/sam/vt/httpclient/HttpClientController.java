2f85b02c2a7f3bb08b9c8123c2588782fbb17521
DangKunpeng <kunpeng.dang@accenture.cn>
2025-12-12 10:46:32 +0800

    #httpClient5

--- src/main/java/com/sam/vt/httpclient/HttpClientController.java
+++ src/main/java/com/sam/vt/httpclient/HttpClientController.java
@@ -33,7 +33,7 @@ public class HttpClientController {
     @PostMapping("/dict")
     public String dict(@RequestBody DictBean dictBean) {
         log.info("outer dictBean={}", dictBean);
-        // 调用本地接口
+        // 调用远程接口（本类内实现）
         String url = "http://localhost:8080/api/httpClient/dictApi";
         return httpClientService.postJson(url, dictBean);
     }
